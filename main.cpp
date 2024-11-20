14d2d52f2893673ebc86c5c7e2af0263b0544ac7
Evelina <sunevelin@gmail.com>
2024-11-20 15:53:05 +0500

    каркас программы

--- main.cpp
+++ main.cpp
@@ -1,9 +1,41 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
 
 using namespace std;
 
-int main()
+// Функция №1: чтение строк из файла в вектор
+void read(const string& file, vector<string>& lines)
 {
-    cout << "Hello world!" << endl;
+
+}
+
+// Функция №2: вывод строк на экран
+void display(const vector<string>& lines)
+{
+
+}
+
+// Функция №3: запись строк в файл (создаёт файл, если его нет)
+void write(const string& file, const vector<string>& lines)
+{
+
+}
+
+int main() {
+    string inputFile = "1.txt";
+    string outputFile = "2.txt";
+    vector<string> lines;
+
+    // Вызов функции №1: чтение строк из файла
+    read(inputFile, lines);
+
+    // Вызов функции №2: вывод строк на экран
+    display(lines);
+
+    // Вызов функции №3: запись строк в файл (создаёт файл, если его нет)
+    write(outputFile, lines);
+
     return 0;
 }
